25718c8031032f72ae2ebfe140c48d70226976bf
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-22 12:01:53 -0700

    Create Main App Component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx'
+
+// convert to hydrate upon implementing SSR
+ReactDOM.render(<App/>, document.getElementById('app'));
 
 
