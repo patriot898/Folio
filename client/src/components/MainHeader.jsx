58ca494e6d7b6094d56c9992625a675d1292e62f
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-23 17:06:01 -0700

    Fix margins on main header

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -4,7 +4,6 @@ const MainHeader = () => {
   return (
     <header id="main-header">
       <div id="top-section">
-        <div id="personal-container">
           <div id="avatar-container">
             <img id="avatar-image" src="headshot-avatar-bw.jpg"></img>
           </div>
@@ -12,17 +11,16 @@ const MainHeader = () => {
             <p className="personal-name">Kiefer Ragay</p>
             <p className="personal-job">full stack software engineer</p>
             <dl className="contact-table">
-              <dt className="contact-term">AGE</dt>
+              <dt className="contact-term">AGE:</dt>
               <dd className="contact-descrip">28</dd>
-              <dt className="contact-term">PHONE</dt>
+              <dt className="contact-term">PHONE:</dt>
               <dd className="contact-descrip">+1 (315) 274-2860</dd>
-              <dt className="contact-term">EMAIL</dt>
+              <dt className="contact-term">EMAIL:</dt>
               <dd className="contact-descrip">kiefer.m.ragay@gmail.com</dd>
-              <dt className="contact-term">LOCATION</dt>
+              <dt className="contact-term">LOCATION:</dt>
               <dd className="contact-descrip">San Francisco, CA</dd>
             </dl>
           </div>
-        </div>
       </div>
     </header>
   )
