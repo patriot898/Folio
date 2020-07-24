3234bbd57e9cf1c5965a114bbaee704e478f314e
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-24 15:42:29 -0700

    Create Separate CSS for Custom Bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../public/styles.css';
+import './customBootstrap.css';
 
 // convert to hydrate upon implementing SSR
 ReactDOM.render(<App />, document.getElementById('app'));
