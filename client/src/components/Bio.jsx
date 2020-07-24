59fe7a4474be0bc04519c638bc2390e0eb51d141
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-23 20:11:17 -0700

    Create Skeleton for Bio Box

---
+++ client/src/components/Bio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Bio() {
+  return (
+    <div class="white-container">
+      <p class="bio-intro">Hello</p>
+      <p class="bio-text"></p>
+    </div>
+  )
+}
+
