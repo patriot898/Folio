cb1918aef0f627f68dcc668f3a5aa3bd1cc82ba7
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-25 10:16:22 -0700

    Repair eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  "extends": "airbnb-base"
-}
+  extends: ['airbnb-base', 'airbnb/hooks'],
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+
+};
