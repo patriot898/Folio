6f3598504d9b30c2a7141a673fe1f33b18b79af3
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-25 15:10:45 -0700

    Create Structure for Resume Blocks

--- client/src/sections/Resume.jsx
+++ client/src/sections/Resume.jsx
@@ -3,15 +3,27 @@ import React from 'react';
 export default function Resume() {
   return (
     <section className="white-section container">
+
       <h2 className="section-title title-text">Resume</h2>
       <p className="general-text">
-      I am an Army Signal Corps Veteran turned Full Stack Software Engineer. I love
-      developing robust and modern web and mobile applications while translating
-      design and theory to practice. My past experience as a military officer imbued
-      me with the communication and management skills absolutely essential to be an
-      effective leader and team player.
+        Since childhood, I've always wanted to be an engineer. Before enrolling in a
+        military academy to secure an engineering degree, I had no clue how oriented
+        the role as an officer would be to project management and leadership!
+        Following my honorable separation from the military, I chose to continue my
+        pursuit to become an engineer and build functional applications and websites
+        with just a laptop and an internet connection...and I love it!
       </p>
-      <h3 className="title-text small-title">Education</h3>
+      <div className="resume-container">
+        <h3 className="title-text small-title">Education</h3>
+        <div className="resume-block">
+          <p className="resume-entry-title">Hack Reactor</p>
+          <p className="resume-entry-date">2020</p>
+
+
+        </div>
+
+      </div>
+
     </section>
   );
 }
