c59ec1680c96b7e063fb802cbad10fd1834e6400
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-25 09:16:52 -0700

    Complete DownloadButton

--- client/src/components/Bio.jsx
+++ client/src/components/Bio.jsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-
+import DownloadButton from './DownloadButton.jsx';
 
 export default function Bio() {
   return (
 
     <section id="bio" className="white-section container">
-      {/* <style type="text/css">
-        {
-          `.btn-download {
-          background-color: rgba(213, 216, 55, .95);
-          color: white;
-          }`
-        }
-      </style> */}
       <h2 id="bio-header" className="section-title">Hello</h2>
       <p className="general-text">I am an Army Signal Corps Veteran turned Full Stack Software Engineer. I love developing robust and modern web and mobile applications while translating design and theory to practice. My past experience as a military officer imbued me with the communication and management skills absolutely essential to be an effective leader and team player. </p>
-      <Button variant="download" size="large">
+      <DownloadButton />
 
-        Download Resume
-        </Button>
+
     </section>
   )
 }
--- client/src/components/DownloadButton.jsx
+++ client/src/components/DownloadButton.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function DownloadButton() {
+  return (
+    <Button href="resume.pdf" download="Ragay_Resume.pdf" variant="download" size="large">
+      <div>
+        <span className="svg-container">
+          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-download hover-download" width="20" height="20" viewBox="0 0 28 28" strokeWidth="3" stroke="#FFFFFF" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+            <polyline points="7 11 12 16 17 11" />
+            <line x1="12" y1="4" x2="12" y2="16" />
+          </svg>
+        </span>
+        <span className="download-text">DOWNLOAD RESUME</span>
+
+      </div>
+    </Button>
+  )
 
 }
