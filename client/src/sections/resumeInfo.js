54c4ee25a586ff65a57782111691e397c8518faa
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-07-25 18:38:54 -0700

    Create File for Storing Resume Info

---
+++ client/src/sections/resumeInfo.js
@@ -0,0 +1,28 @@
+const skills = [];
+const educationTimeline = [
+  {
+    title: 'Hack Reactor, San Francisco, California',
+    date: '2020',
+    description: 'Full Stack Software Engineering Immersive',
+  },
+  {
+    title: 'United States Military Academy, West Point, New York',
+    date: '2010 - 2014',
+    description: 'Bachelor of Science, Electrical Engineering',
+  },
+];
+
+const experienceTimeline = [
+  {
+    title: 'Hack Reactor, San Francisco, California',
+    date: '2020',
+    description: 'Full Stack Software Engineering Immersive',
+  },
+  {
+    title: 'United States Military Academy, West Point, New York',
+    date: '2010 - 2014',
+    description: 'Bachelor of Science, Electrical Engineering',
+  },
+];
+
+export default { skills, educationTimeline, experienceTimeline };
